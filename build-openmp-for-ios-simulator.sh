cba2fb8d802227a5e211794e15c0b5852286f214
Alex Wang <aw1621107@gmail.com>
2016-09-23 10:47:25 -0400

    Add script to build i386 libomp.dylib
    dylib still links against /usr/lib/libSystem.dylib, so not sure if this
    is totally correct.
    
    .gitignore was added to ignore the generated build/ directory containing
    the CMake out-of-source openmp build.
    
    .gitmodules was edited because the openmp build copies its results to
    the openmp repo runtime/exports directory, which makes the submodule
    dirty to git. The submodule shouldn't have its sources changed, so git
    shouldn't need to worry about anything that happens in there.

---
+++ build-openmp-for-ios-simulator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[[ ! -d openmp/.git ]] && git submodule update --init --recursive
+
+[[ ! -d build/ ]] && mkdir build
+cd build
+
+cmake ../openmp -DCMAKE_CROSSCOMPILING=True -DCMAKE_OSX_ARCHITECTURES=i386 \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -mios-simulator-version-min=8.0" \
+      -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -mios-simulator-version-min=8.0" \
+      -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
+
+make -j 4

1611cfc91e089395f1f1fa9eb4149798124f1d49
Alex Wang <aw1621107@gmail.com>
2016-09-23 19:08:26 -0400

    Create fat binaries instead of just i386
    fat --> i386 + x86_64. Might have to add various ARM archs later...

--- build-openmp-for-ios-simulator.sh
+++ build-openmp-for-ios-simulator.sh
@@ -5,7 +5,7 @@
 [[ ! -d build/ ]] && mkdir build
 cd build
 
-cmake ../openmp -DCMAKE_CROSSCOMPILING=True -DCMAKE_OSX_ARCHITECTURES=i386 \
+cmake ../openmp -DCMAKE_CROSSCOMPILING=True -DCMAKE_OSX_ARCHITECTURES="x86_64;i386" \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -mios-simulator-version-min=8.0" \
       -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -mios-simulator-version-min=8.0" \
       -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
